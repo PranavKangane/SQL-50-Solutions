7204752eb284834d2299ce8440efe6c334c2ffd8
coldrun <53134516+PranavKangane@users.noreply.github.com>
2025-07-13 10:22:49 -0400

    1633-percentage-of-users-attended-a-contest .sql 07-13-2025

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -26,6 +26,15 @@ ORDER BY 2 DESC, 1
 
 
 /*
+Appraoch
+1. Created a main base where I plotted the contest ids from register table against all user ids in the Users.
 
+2. For every contest id, calculated the unique user ids using the prev cte 'main'
+
+3. Calculated the total users from the users table.
+
+4. In 'final' cte again created an outer join to plot the total users against every record. The number is going to be same for all records.
+
+5. Calculated the percentage and filtered those rows where contest ids can be null.
 
 */
