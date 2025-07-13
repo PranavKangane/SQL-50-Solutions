063a25eebf93f21711f080eb2d7b3c3f0fe52ec6
coldrun <53134516+PranavKangane@users.noreply.github.com>
2025-07-13 08:58:43 -0400

    1280-students-and-examinations .sql 07-13-2025

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -16,3 +16,21 @@ ON A.student_id = B.student_id
 AND A.subject_name = B.subject_name
 ORDER BY 1,2,3,4 DESC
 
+
+
+/*
+Approach
+1. Main base will have ALl students and subjects against their name irrespective if they have taken the exam or not. This is created using cross join. 
+4 RECORDS from students and 3 from subjects will generate (4*3) records.
+
+2. Exam given CTE will calculate the total exam given by a student based count of student id, subject name as a group
+
+
+3. Will combine Main base with exam given using left join because we want to display info for all student. 
+
+4. Replacing records with null values with 0 using IFNULL function in select statement
+
+5. Sorting based on ids, name, subject and then exams given in desc order.
+
+
+*/
